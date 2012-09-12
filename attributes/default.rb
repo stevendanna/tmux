628c7a95a0cbf95226c1b4fb8fd60c36aec4bcf6
Sean Gallagher <sean@torandu.com>
2012-09-12 15:45:58 -0700

    [COOK-1649] Fix typo in default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default['tmux']['version'] = '1.6'
-default['tmxu']['checksum'] = '8756f6bcecb18102b87e5d6f5952ba2541f68ed3'
+default['tmux']['checksum'] = '8756f6bcecb18102b87e5d6f5952ba2541f68ed3'
