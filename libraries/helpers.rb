e82f4c31269b64d2bb7552a03f67ee8965c255a0
Steven Danna <steve@chef.io>
2015-08-29 16:01:14 +0100

    Dynamically choose version based on platform support
    tmux 1.7 and greater require libevent2 or libevent1.14.1b+.
    Unfortunately, most platforms don't yet ship with those libraries.
    Managing libevent2 on the system is a bit beyond the scope of this
    cookbook at the moment.

---
+++ libraries/helpers.rb
@@ -0,0 +1,33 @@
+module TmuxCookbook
+  module Helpers
+    LATEST_VERSION='2.0'
+    LATEST_CHECKSUM='795f4b4446b0ea968b9201c25e8c1ef8a6ade710ebca4657dd879c35916ad362'
+    FALLBACK_VERSION='1.6'
+    FALLBACK_CHECKSUM='faee08ba1bd8c22537cd5b7458881d1bdb4985df88ed6bc5967c56881a7efbd6'
+
+    def self.auto_version(node)
+      if libevent2_available?(node)
+        LATEST_VERSION
+      else
+        FALLBACK_VERSION
+      end
+    end
+
+    def self.auto_checksum(node)
+      if libevent2_available?(node)
+        LATEST_CHECKSUM
+      else
+        FALLBACK_CHECKSUM
+      end
+    end
+
+    def self.libevent2_available?(node)
+      case node['platform_family']
+      when 'rhel'
+        Gem::Version.new(node['platform_version']) >= Gem::Version.new('7.0')
+      when 'debian'
+        Gem::Version.new(node['platform_version']) > Gem::Version.new('12.04')
+      end
+    end
+  end
+end
--- test/integration/source/bats/verify_installed.bats
+++ test/integration/source/bats/verify_installed.bats
@@ -1,3 +1,4 @@
 @test "it installs installed the tmux binary" {
+  export PATH=$PATH:/usr/local/bin
   which tmux
 }
